259d5b78b2660a7df5a5ecabaa490ca9ee180369
Kallistina <despotidou02@gmail.com>
2026-02-04 14:00:17 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React from "react";
 import Content from "./components/content";
-import Frontpageanimation from "./components/frontpageanimation";
 import Navbar from "./components/navbar";
-import { BrowserRouter, Route,Link,Routes } from 'react-router-dom'
 import About from "./components/about";
-import Skills from "./components/skills"
+import Skills from "./components/skills";
 import Contact from "./components/contact";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Navbar/>
-      <BrowserRouter>
-      <Routes>
-        <Route path='/' Component={Content} />
-        <Route path='/about' Component={About} />
-        <Route path='/skills' Component={Skills} />
-        <Route path='/contact' Component={Contact} />
-      </Routes>
-      </BrowserRouter>
-        
+      <Navbar />
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Content />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </HashRouter>
     </div>
   );
 }
